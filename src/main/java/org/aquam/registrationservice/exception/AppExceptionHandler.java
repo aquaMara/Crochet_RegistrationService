2939ca990c3c7d173c58ad3f78cf035942d87f87
Marina Tchernyshenko <aquamarina.na@gmail.com>
2022-08-23 13:52:59 +0300

    feat: Add exception handler

---
+++ src/main/java/org/aquam/registrationservice/exception/AppExceptionHandler.java
@@ -0,0 +1,48 @@
+package org.aquam.registrationservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<AppResponse> handleEntityExistsException(PersistenceException exception) {
+        AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);    // 404
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<AppResponse> handleEntityNotFoundException(PersistenceException exception) {
+        AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);    // 404
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<AppResponse> handleIllegalStateException(RuntimeException exception) {
+        AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<AppResponse> handleIOException(Exception exception) {
+        AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);     // 500
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<AppResponse> handleConstraintViolationException(ConstraintViolationException exception) {
+        AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.BAD_REQUEST, exception.getConstraintViolations());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
