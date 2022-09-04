ffac262c4c41bda3bf00d1acdca8e38f9e6cc407
Marina Tchernyshenko <aquamarina.na@gmail.com>
2022-09-05 00:25:14 +0300

    feat: Add new exception handling

--- src/main/java/org/aquam/registrationservice/exception/AppExceptionHandler.java
+++ src/main/java/org/aquam/registrationservice/exception/AppExceptionHandler.java
@@ -19,24 +19,24 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({EntityExistsException.class, IllegalStateException.class})
     public ResponseEntity<AppResponse> handleBadRequest(RuntimeException exception) {
         AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);    // 404
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<AppResponse> handleConstraintViolationException(ConstraintViolationException exception) {
         AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.BAD_REQUEST, exception.getConstraintViolations());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);    // 404
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<AppResponse> handleNotFound(PersistenceException exception) {
         AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);    // 404
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(IOException.class)
+    @ExceptionHandler({IOException.class, EmailSendingException.class})
     public ResponseEntity<AppResponse> handleInternalServerError(Exception exception) {
         AppResponse response = new AppResponse(exception.getMessage(), ZonedDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);     // 500
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
